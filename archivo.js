847db8512ad7f12baeee17d955ad37f9e3907abe
f14v1u5 <flaarivero96@gmail.com>
2025-06-04 18:07:27 -0300

    Actividades del 03deJun

--- archivo.js
+++
@@ -1,8 +0,0 @@
-var nombre = 'flavio';
-var Nombre = 'Candela';
-var _nombre = '_nicolas';
-var NOMBRE = 'ROCIO';
-var $_nombre = '$_jose';
-
-let saludo = 'hola'
-document.write($_nombre);
---
+++ js/funciones_matematicas.js
@@ -0,0 +1,70 @@
+//Redondea dependiendo la tendencia
+var precio = Math.round(399.43);
+document.write('Precio redondeado: ',precio);
+document.write('<br>');
+
+
+//Redondea hacia arriba
+var precio = Math.ceil(299.9);
+document.write('Precio redondeado: ', precio);
+document.write('<br>');
+
+
+//Redondea hacia abajo
+var precio = Math.floor(540.9);
+document.write('Precio redondeado: ',precio);
+document.write('<br>');
+
+
+//Seno, Coseno y Tangente
+var seno = Math.sin(45);
+document.write('Seno de 45: ',seno);
+document.write('<br>');
+
+
+//Exponencial de un numero
+var expo = Math.exp(2);
+document.write('Exponencial de 2: ',expo);
+document.write('<br>');
+
+
+//Logaritmo
+var logaritmo = Math.log(10);
+document.write('Logaritmo de 10: ',logaritmo);
+document.write('<br>');
+
+
+//Valor absoluto
+var absoluto = Math.abs(-10);
+document.write('Valor absoluto de -10: ',absoluto);
+document.write('<br>');
+
+
+//Valor maximo de una secuencia
+var maximo = Math.max(10,50,600,1,8);
+document.write('Mayor valor: ',maximo);
+document.write('<br>');
+
+
+//Valor minimo de una secuencia
+var minimo = Math.min(10,50,600,1,8);
+document.write('Menor valor: ',minimo);
+document.write('<br>');
+
+
+//Valor aleatorio
+var aleatorio = Math.round(Math.random()*10);
+document.write('Valor aleatorio: ',aleatorio);
+document.write('<br>');
+
+
+//Trae la raiz cuadrada de un numero
+var valor = Math.sqrt(81);
+document.write('Raiz de 81: ',valor);
+document.write('<br>');
+
+
+//Exponente de un numero
+var exponente = Math.pow (4,2);
+document.write('Valor de 4 a la 2: ',exponente);
+document.write('<br>');
